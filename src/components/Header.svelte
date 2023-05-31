0dda287a3cdbdcf1561c955f853a566af58f7b46
MelvDouc <melv.douc@gmail.com>
2023-06-01 00:03:09 +0200

    Set up Bootstrap

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,88 +1,57 @@
+<script lang="ts">
+  import HeaderDropdown from "$components/HeaderDropdown.svelte";
+  export let title: string;
+</script>
+
 <header>
-  <section class="header-left">
-    <img
-      src="http://thionville-echecs.fr/wp-content/uploads/2022/05/cropped-logo-blanc2x-192x192.png"
-      alt="Logo"
-      width="30"
-      height="30"
-    />
-    <h1>Thionville Échecs — Équipes</h1>
-  </section>
-  <section class="header-right">
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Accueil</a>
-        </li>
-        <li>
-          <a href="/matchs">Matchs</a>
-        </li>
-        <li>
-          <a href="/joueurs">Joueurs</a>
-        </li>
-      </ul>
-    </nav>
-  </section>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a href="/" class="navbar-brand d-flex align-items-center gap-2">
+        <img
+          src="http://thionville-echecs.fr/wp-content/uploads/2022/05/cropped-logo-blanc2x-192x192.png"
+          alt="Logo"
+          width={30}
+          height={24}
+          class="d-inline-block align-text-top"
+        />
+        <h1 class="m-0 white-space-pre-wrap fs-5">{title}</h1>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarText"
+      >
+        <span class="navbar-toggler-icon" />
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <HeaderDropdown mainText="Matchs">
+            <li>
+              <a href="/matchs" class="dropdown-item">Matchs</a>
+            </li>
+            <li>
+              <a href="/matchs/nouveau" class="dropdown-item">Ajouter un match</a>
+            </li>
+          </HeaderDropdown>
+          <HeaderDropdown mainText="Joueurs">
+            <li>
+              <a href="/joueurs" class="dropdown-item">Liste</a>
+            </li>
+            <li>
+              <a href="/joueurs/nouveau" class="dropdown-item">Ajouter un joueur</a>
+            </li>
+          </HeaderDropdown>
+        </ul>
+        <span class="navbar-text">
+          <div class="d-flex align-items-center gap-2">
+            <!-- log out button -->
+          </div>
+        </span>
+      </div>
+    </div>
+  </nav>
 </header>
-
-<style lang="scss" scoped>
-  header {
-    $bg-clr1: hsl(0, 0%, 20%);
-    $bg-clr2: hsl(0, 0%, 13%);
-    $bs-clr: hsl(0, 0%, 16%);
-    padding: 0.8em;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    gap: 1em;
-    background: hsl(0, 0%, 13%);
-    background: linear-gradient(to bottom, $bg-clr1 0%, $bg-clr2 100%);
-    box-shadow: inset 0 1px 2px $bs-clr, inset 0 -1px 2px $bs-clr;
-    color: white;
-
-    section {
-      max-width: 100vw;
-    }
-
-    h1 {
-      word-break: break-word;
-      font-size: 1.2rem;
-
-      @media only screen and (width >= 400px) {
-        font-size: 1.5rem;
-      }
-
-      @media only screen and (width >= 600px) {
-        font-size: 1.75rem;
-      }
-    }
-  }
-
-  .header-left {
-    display: flex;
-    align-items: center;
-    gap: 1em;
-  }
-
-  .header-right {
-    nav {
-      ul {
-        list-style: none;
-        display: flex;
-        align-items: center;
-        gap: 0.75em;
-
-        a {
-          text-decoration: none;
-          color: inherit;
-          transition: color 200ms ease-out;
-
-          &:hover {
-            color: gold;
-          }
-        }
-      }
-    }
-  }
-</style>
