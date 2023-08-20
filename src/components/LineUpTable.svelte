295633291c9b5cddc7eb1b09b429519ea35b06a5
MelvDouc <melv.douc@gmaiL.com>
2023-08-20 20:24:37 +0200

    Add table wrapper

--- src/components/LineUpTable.svelte
+++ src/components/LineUpTable.svelte
@@ -58,62 +58,64 @@
   }
 </script>
 
-<table class="table table-warning table-striped table-hover">
-  <thead>
-    <tr>
-      <th>Éch.</th>
-      <th>Prénom NOM</th>
-      <th>Code FFE</th>
-      <th>Elo</th>
-      <th class="text-center">Cap.</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#each Object.entries($lineUpStore) as [board, item]}
+<div class="tableWrapper">
+  <table class="table table-warning table-striped table-hover">
+    <thead>
       <tr>
-        <td>{+board}{(+board % 2 === 1) === whiteOnOdds ? "B" : "N"}</td>
-        <td>
-          <input
-            type="text"
-            value={item?.name ?? ""}
-            on:input={getUpdateByNameFn(+board)}
-            list="players-datalist"
-          />
-        </td>
-        <td>
-          <input
-            type="text"
-            class="ffeId-input"
-            value={item?.ffeId ?? ""}
-            on:input={getUpdateFfeIdFn(+board)}
-          />
-        </td>
-        <td>
-          <input
-            type="number"
-            class="rating-input"
-            value={item?.rating || ""}
-            on:input={getUpdateRatingFn(+board)}
-          />
-        </td>
-        <td class="text-center">
-          <input
-            type="radio"
-            name="captain-ffe-id"
-            checked={captainFfeId !== null && item?.ffeId === captainFfeId}
-            on:change={getCheckedHandler(+board)}
-          />
-        </td>
+        <th>Éch.</th>
+        <th>Prénom NOM</th>
+        <th>Code FFE</th>
+        <th>Elo</th>
+        <th class="text-center">Cap.</th>
       </tr>
-    {/each}
-  </tbody>
+    </thead>
+    <tbody>
+      {#each Object.entries($lineUpStore) as [board, item]}
+        <tr>
+          <td>{+board}{(+board % 2 === 1) === whiteOnOdds ? "B" : "N"}</td>
+          <td>
+            <input
+              type="text"
+              value={item?.name ?? ""}
+              on:input={getUpdateByNameFn(+board)}
+              list="players-datalist"
+            />
+          </td>
+          <td>
+            <input
+              type="text"
+              class="ffeId-input"
+              value={item?.ffeId ?? ""}
+              on:input={getUpdateFfeIdFn(+board)}
+            />
+          </td>
+          <td>
+            <input
+              type="number"
+              class="rating-input"
+              value={item?.rating || ""}
+              on:input={getUpdateRatingFn(+board)}
+            />
+          </td>
+          <td class="text-center">
+            <input
+              type="radio"
+              name="captain-ffe-id"
+              checked={captainFfeId !== null && item?.ffeId === captainFfeId}
+              on:change={getCheckedHandler(+board)}
+            />
+          </td>
+        </tr>
+      {/each}
+    </tbody>
 
-  <datalist id="players-datalist">
-    {#each [...map.keys()] as name}
-      <option value={name}>{name}</option>
-    {/each}
-  </datalist>
-</table>
+    <datalist id="players-datalist">
+      {#each [...map.keys()] as name}
+        <option value={name}>{name}</option>
+      {/each}
+    </datalist>
+  </table>
+</div>
 
 <style scoped>
   .rating-input {
--- src/routes/(main)/matchs/[season]/+page.svelte
+++ src/routes/(main)/matchs/[season]/+page.svelte
@@ -16,53 +16,58 @@
 
 <p class="mb-5">Tous les matchs commencent à <strong>14h15</strong>.</p>
 
-<table class="table table-light table-striped">
-  <thead>
-    <tr>
-      <th>Rd.</th>
-      <th>Adversaire</th>
-      <th>Adresse</th>
-      <th>Date</th>
-      <th>Actions</th>
-    </tr>
-  </thead>
-  {#each data.matches as item}
-    <tbody class="subtitleTbody">
+<div class="tableWrapper">
+  <table class="table table-light table-striped">
+    <thead>
       <tr>
-        <td colspan={5} class="text-center">{item.teamName}</td>
+        <th>Rd.</th>
+        <th>Adversaire</th>
+        <th>Adresse</th>
+        <th>Date</th>
+        <th>Actions</th>
       </tr>
-    </tbody>
-    <tbody>
-      {#each item.matches as m}
+    </thead>
+    {#each data.matches as item}
+      <tbody class="subtitleTbody">
         <tr>
-          <td>{m.round}</td>
-          <td>{m.opponent}</td>
-          <td>
-            <address>
-              <div>{m.address}</div>
-              <div>{m.zipCode} {m.city}</div>
-            </address>
-          </td>
-          <td>{formatDate(m.date)}</td>
-          <td class="align-middle">
-            <div class="d-flex justify-content-center align-items-center gap-2">
-              <a href="/matchs/{m.season}/{m.round}/{m.teamName}/modifier" class="btn btn-primary">
-                <i class="bi bi-pen-fill" />
-              </a>
-              <a
-                href="/matchs/{m.season}/{m.round}/{m.teamName}/composition"
-                class="btn btn-warning"
-                target="_blank"
-              >
-                <i class="bi bi-table" />
-              </a>
-            </div>
-          </td>
+          <td colspan={5} class="text-center">{item.teamName}</td>
         </tr>
-      {/each}
-    </tbody>
-  {/each}
-</table>
+      </tbody>
+      <tbody>
+        {#each item.matches as m}
+          <tr>
+            <td>{m.round}</td>
+            <td>{m.opponent}</td>
+            <td>
+              <address>
+                <div>{m.address}</div>
+                <div>{m.zipCode} {m.city}</div>
+              </address>
+            </td>
+            <td>{formatDate(m.date)}</td>
+            <td class="align-middle">
+              <div class="d-flex justify-content-center align-items-center gap-2">
+                <a
+                  href="/matchs/{m.season}/{m.round}/{m.teamName}/modifier"
+                  class="btn btn-primary"
+                >
+                  <i class="bi bi-pen-fill" />
+                </a>
+                <a
+                  href="/matchs/{m.season}/{m.round}/{m.teamName}/composition"
+                  class="btn btn-warning"
+                  target="_blank"
+                >
+                  <i class="bi bi-table" />
+                </a>
+              </div>
+            </td>
+          </tr>
+        {/each}
+      </tbody>
+    {/each}
+  </table>
+</div>
 
 <style lang="scss">
   .subtitleTbody {
